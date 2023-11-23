49be64553a4c8cf9827204aa560252dd177617a2
billkolos <billooko2@gmail.com>
2023-11-23 14:41:40 -0800

    node journey

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bill Clinton Ooko <https://github.com/Billkolos>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
